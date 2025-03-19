63311d034749f2d3f450d1203d047f372250fd7e
NathanNgatia <ivenyos@gmail.com>
2025-03-19 10:15:11 +0300

    add initial project structure with assets, configuration files, and core components

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="footer-bar">
+    <p> &copy; 2025 WomenDigi. All rights reserved.</p>
+    </footer>
+    <RouterView/>
+  </template>
+<style scoped>
+  .footer-bar {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import TestPage from '../views/TestPage.vue'
+import ContactUsView from '../views/ContactUs.vue'
+import LoginPage from '../views/LoginPage.vue'
+import SignUpPage from '../views/SignUp.vue'
+import WelcomePage from '../views/WelcomePage.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about-us',
+      name: 'about',      
+      component: AboutView
+    },
+    {
+      path: '/test',
+      name: 'TestPage',
+      component: TestPage
+    },
+    {
+      path: '/login',
+      name: 'LoginPage',
+      component: LoginPage
+    },
+    {
+      path: '/sign-up',
+      name: 'SignUpPage',
+      component: SignUpPage
+    },
+    {
+      path: '/contact-us',
+      name: 'ContactUs',
+      component: ContactUsView
+    },
+    {
+      path: '/welcome',
+      name: 'Welcome',
+      component: WelcomePage
+    }
+  ]
+})
+
+export default router
