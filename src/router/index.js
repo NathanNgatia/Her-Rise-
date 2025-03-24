dc203e2940f6c3650cbc2f0b6203e611966466ef
NathanNgatia <ivenyos@gmail.com>
2025-03-24 13:00:37 +0300

    fourth commit

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'Home',
-      component: () => imoort('../views/HomeView.vue')
+      component: () => import('../views/HomeView.vue')
     },
     {
       path: '/about-us',
